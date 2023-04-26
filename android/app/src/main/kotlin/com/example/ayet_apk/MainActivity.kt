01d1db6d786ad1c0722f16168421b08539faa484
Cubeiik <jakub.kubik71@gmail.com>
2023-04-26 15:06:03 +0200

    ayet

---
+++ android/app/src/main/kotlin/com/example/ayet_apk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ayet_apk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:ayet_apk/custom_offerwall_show.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Ayet test apk'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void runAyet(int platform, String id) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WebViewExample(
+          platform: platform, //if platform = 1 - ios
+          identifier: id,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool iphone = Theme.of(context).platform == TargetPlatform.iOS;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                final int platformId = iphone ? 1 : 0;
+                runAyet(platformId, '124523452');
+              },
+              child: const Text('AyetStudios'))),
+    );
+  }
+}
